64ac3c2994f932ed8058237453aad4f970a1cd08
Alan-Velasco <vga2696@gmail.com>
2023-02-05 20:02:43 -0600

    Agregue gráficos a Alumno siguiendo el documento de word, use googleCharts

---
+++ aplicacionPrincipal/jsAlumnos/ControllCharts.js
@@ -0,0 +1,29 @@
+/*
+OBJETIVO: Si configuras la opción is3D en true, el gráfico circular se dibujará como si tuviera tres dimensiones:
+
+AUTORES: ALAN MITCHELL VELASCO GONZÁLEZ, GERARDO ITURRIBARIA MOYA 
+
+*/
+
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          width: 600,
+          height: 500,
+          title: 'My Daily Activities',
+          is3D: true,//HACER GRÁFICA 3D
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
---
+++ aplicacionPrincipal/jsAlumnos/ControllCharts3.js
@@ -0,0 +1,29 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawMaterial);
+
+function drawMaterial() {
+      var data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population', '2000 Population'],
+        ['New York City, NY', 8175000, 8008000],
+        ['Los Angeles, CA', 3792000, 3694000],
+        ['Chicago, IL', 2695000, 2896000],
+        ['Houston, TX', 2099000, 1953000],
+        ['Philadelphia, PA', 1526000, 1517000]
+      ]);
+
+      var materialOptions = {
+        chart: {
+          title: 'Population of Largest U.S. Cities'
+        },
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'City'
+        },
+        bars: 'Vertical' //Horizontal
+      };
+      var materialChart = new google.charts.Bar(document.getElementById('chart_div'));
+      materialChart.draw(data, materialOptions);
+    }
