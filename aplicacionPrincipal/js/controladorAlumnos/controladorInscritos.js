d0a4d473fa33f24ec4e716e46449d2d68c9c1407
gmoya110101 <giturribarriam@gmail.com>
2023-02-08 11:56:05 -0600

    Nuevos paquetes

---
+++ aplicacionPrincipal/js/controladorAlumnos/controladorInscritos.js
@@ -0,0 +1,34 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+$.ajax({
+  url: "controlador/controlador_grafico.php",
+  method: "POST",
+  dataType: "JSON",
+  success: function (data) {
+    drawChart(data);
+  }
+});
+
+function drawChart(chart_data) {
+  var jsonData = chart_data;
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Carrera');
+  data.addColumn('number', 'total');
+
+  $.each(jsonData, function (i, jsonData) {
+    var carrera = jsonData.carrera;
+    var total = parseInt($.trim(jsonData.total));
+    data.addRows([[carrera, total]]);
+  });
+
+  var options = {
+    width: 700,
+    height: 500,
+    title: 'Alumnos inscritos por carrera',
+    is3D: true,//HACER GRÁFICA 3D
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
